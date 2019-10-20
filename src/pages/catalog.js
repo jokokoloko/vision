d4d860cf40b4907d2977e3ea422f882934a44e8c
jokokoloko <jhowland82@gmail.com>
2019-10-20 01:42:07 -0700

    Updated catalog.js

--- src/pages/catalog.js
+++ src/pages/catalog.js
@@ -1,14 +1,81 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import { logicDescription } from '../logic';
+import * as path from '../path';
 import Layout from '../components/Layout';
-import Basic from '../components/section/Basic';
+import Feed from '../components/section/Feed';
 import Link from '../components/unit/Link';
 
-export default ({ location }) => (
-    <Layout template="not-found" title="404: Not Found" description="This page does not exist." location={location}>
-        <Basic id="not-found" space="space-custom">
-            <header>
-                <h1>Catalog</h1>
-            </header>
-        </Basic>
-    </Layout>
-);
+export default ({ location, data }) => {
+    const { products, page } = data;
+    const loopProduct = products.edges.map(({ node }) => {
+        const {
+            images: [firstImage],
+            variants: [firstVariant],
+        } = node;
+        return (
+            <article key={node.id} id={`product-${node.handle}`} className="product col-lg-4">
+                <div className="case relative node-xs-50">
+                    <figure className="node-xs-50">
+                        <img className="img-fluid" src={firstImage.originalSrc} alt={node.title} />
+                    </figure>
+                    <header className="node-xs-50">
+                        <h3>
+                            <Link className="stretched-link" to={path.PRODUCT === '/' ? `/${node.handle}` : `${path.PRODUCT}/${node.handle}`}>
+                                {node.title}
+                            </Link>
+                        </h3>
+                        <p className="price">${firstVariant.price}</p>
+                    </header>
+                </div>
+                <div className="case node-xs-50">
+                    <footer>
+                        <button type="button" className="btn btn-default btn-lg btn-initial no-class">
+                            Add to cart
+                        </button>
+                    </footer>
+                </div>
+            </article>
+        );
+    });
+    return (
+        <Layout template={`page page-${page.slug}`} title={page.title} description={logicDescription(page)} location={location}>
+            {page && products.edges.length > 0 && (
+                <Feed id={`feed-${page.slug}`} space="space-custom" item="product">
+                    {page.head && (
+                        <header
+                            className="copy node-xs-50 node-lg-80 text-lg-center"
+                            dangerouslySetInnerHTML={{ __html: page.head.childMarkdownRemark.html }}
+                        />
+                    )}
+                    <section className="node-xs-50 node-lg-80 cheat-both">
+                        <div className="row gutter-80">{loopProduct}</div>
+                    </section>
+                </Feed>
+            )}
+        </Layout>
+    );
+};
+
+export const query = graphql`
+    query pageCatalog {
+        products: allShopifyProduct(sort: { fields: publishedAt, order: DESC }) {
+            edges {
+                node {
+                    id
+                    handle
+                    title
+                    images {
+                        originalSrc
+                    }
+                    variants {
+                        price
+                    }
+                }
+            }
+        }
+        page: contentfulPage(slug: { eq: "catalog" }) {
+            ...contentPage
+        }
+    }
+`;
--- src/path.js
+++ src/path.js
@@ -1,6 +1,7 @@
 // Main
 export const ROOT = '/';
 export const POST = '/post';
+export const PRODUCT = '/product';
 export const ABOUT = '/about';
 export const OVERVIEW = '/overview';
 export const CATALOG = '/catalog';
