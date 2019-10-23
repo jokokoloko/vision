5a3882bc414e967954668cd968f478cfe39105d1
jokokoloko <jhowland82@gmail.com>
2019-10-23 01:11:57 -0700

    Added initializeCheckout to shop.js

--- src/contexts/shop.js
+++ src/contexts/shop.js
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import Client from 'shopify-buy';
 
 const client = Client.buildClient({
@@ -22,21 +22,32 @@ const defaultValues = {
 export const ShopContext = createContext(defaultValues);
 
 export const ShopProvider = ({ children }) => {
-    const addProductToCart = async (variantId) => {
+    const [checkoutId, setCheckoutId] = useState({});
+    const initializeCheckout = async () => {
         try {
             const newCheckout = await client.checkout.create();
+            setCheckoutId(newCheckout.id);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+    const addProductToCart = async (variantId) => {
+        try {
             const lineItems = [
                 {
                     quantity: 1,
                     variantId,
                 },
             ];
-            const addItems = await client.checkout.addLineItems(newCheckout.id, lineItems);
-            console.log(addItems);
+            const addItems = await client.checkout.addLineItems(checkoutId, lineItems);
+            console.log(addItems.webUrl);
         } catch (e) {
             console.error(e);
         }
     };
+    useEffect(() => {
+        initializeCheckout();
+    }, []);
     return (
         <ShopContext.Provider
             value={{
