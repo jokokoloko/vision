a92fffd310df7249a1d839fc81d69a71fceca3ce
jokokoloko <jhowland82@gmail.com>
2019-10-26 03:51:05 -0700

    Added spring.js

--- src/components/project/SpringSlideCart.js
+++ src/components/project/SpringSlideCart.js
@@ -1,16 +1,13 @@
 import React, { useContext } from 'react';
 import { useTransition, animated } from 'react-spring';
+import * as spring from '../../spring';
 import { ShopContext } from '../../contexts/shop';
 import Cart from './Cart';
 
 const SpringSlideCart = () => {
     const { isCartOpen, onCartClose } = useContext(ShopContext);
-    const transitions = useTransition(isCartOpen, null, {
-        from: { transform: 'translate3d(100%, 0, 0)' },
-        enter: { transform: 'translate3d(0, 0, 0)' },
-        leave: { transform: 'translate3d(100%, 0, 0)' },
-    });
-    return transitions.map(
+    const springSlide = useTransition(isCartOpen, null, spring.slide);
+    return springSlide.map(
         ({ item, key, props }) =>
             item && (
                 <animated.aside key={key} style={props} id="spring-slide-cart" className="spring-slide-cart spring-slide">
---
+++ src/spring.js
@@ -0,0 +1,6 @@
+// Transition - Slide
+export const slide = {
+    from: { transform: 'translate3d(100%, 0, 0)' },
+    enter: { transform: 'translate3d(0, 0, 0)' },
+    leave: { transform: 'translate3d(100%, 0, 0)' },
+};
