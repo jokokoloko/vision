c37fe47fb72f6e89c70f58314915e247fded731e
jokokoloko <jhowland82@gmail.com>
2019-10-26 17:23:34 -0700

    Updated Shroud.js to modify body style for overflow-y

--- src/components/widget/Shroud.js
+++ src/components/widget/Shroud.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTransition, animated } from 'react-spring';
 import PropTypes from 'prop-types';
 import * as spring from '../../spring';
 
 const Shroud = ({ isOpen, onClose }) => {
+    useEffect(() => {
+        isOpen && (document.body.style.overflowY = 'hidden');
+        return () => document.body.removeAttribute('style');
+    }, [isOpen]);
     const springFade = useTransition(isOpen, null, spring.fade);
     const animateSpringFade = springFade.map(
         ({ item, key, props }) => item && <animated.aside key={key} style={props} id="shroud" className="shroud" onClick={onClose} />,
