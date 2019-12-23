a6ec2c300da4bc983e85e0f949f087ce1fc3b41b
jokokoloko <jhowland82@gmail.com>
2019-12-23 11:16:46 -0800

    Updated Loader.js

--- src/components/widget/Loader.js
+++ src/components/widget/Loader.js
@@ -7,41 +7,38 @@ const Loader = () => {
     return (
         <Shroud isOpen={isLoading}>
             <svg width="120" height="15" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg" fill="#fff">
-                <circle cx="15" cy="15" r="15">
-                    <animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15" calcMode="linear" repeatCount="indefinite" />
+                <circle cx="15" cy="15" r="10">
                     <animate
                         attributeName="fill-opacity"
                         from="1"
                         to="1"
                         begin="0s"
-                        dur="0.8s"
-                        values="1;.5;1"
+                        dur="1s"
+                        values="1;.3;.3"
                         calcMode="linear"
                         repeatCount="indefinite"
                     />
                 </circle>
-                <circle cx="60" cy="15" r="9" fillOpacity="0.3">
-                    <animate attributeName="r" from="9" to="9" begin="0s" dur="0.8s" values="9;15;9" calcMode="linear" repeatCount="indefinite" />
-                    <animate
-                        attributeName="fill-opacity"
-                        from="0.5"
-                        to="0.5"
-                        begin="0s"
-                        dur="0.8s"
-                        values=".5;1;.5"
-                        calcMode="linear"
-                        repeatCount="indefinite"
-                    />
-                </circle>
-                <circle cx="105" cy="15" r="15">
-                    <animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15" calcMode="linear" repeatCount="indefinite" />
+                <circle cx="60" cy="15" r="10">
                     <animate
                         attributeName="fill-opacity"
                         from="1"
                         to="1"
                         begin="0s"
-                        dur="0.8s"
-                        values="1;.5;1"
+                        dur="1s"
+                        values=".3;1;.3"
+                        calcMode="linear"
+                        repeatCount="indefinite"
+                    />
+                </circle>
+                <circle cx="105" cy="15" r="10">
+                    <animate
+                        attributeName="fill-opacity"
+                        from="1"
+                        to="1"
+                        begin="0s"
+                        dur="1s"
+                        values=".3;.3;1"
                         calcMode="linear"
                         repeatCount="indefinite"
                     />
