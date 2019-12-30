5bacfbb856d934a1e4ff68f6c0d582801c43842e
jokokoloko <jhowland82@gmail.com>
2019-12-30 00:30:56 -0800

    Updated ArticleFeature.js to use Image component instead of gatsby-image directly

--- src/components/project/ArticleFeature.js
+++ src/components/project/ArticleFeature.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Img from 'gatsby-image';
 import { logicDescription } from '../../logic';
+import Image from '../unit/Image';
 
 const ArticleFeature = ({ feature }) => (
     <article id={`feature-${feature.slug}`} className={`feature feature-${feature.order} col-lg-4`}>
-        <figure className="node-xs-50 d-flex">
-            <Img className="image" fixed={feature.image.fixed} alt={feature.title} />
+        <figure className="node-xs-50 d-flex justify-content-center">
+            <Image className="image" source={feature.image.fixed} alternate={feature.title} fixed />
         </figure>
-        <header className="node-xs-50">
+        <header className="node-xs-50 text-center">
             <h4>{feature.title}</h4>
-            <p className="excerpt" dangerouslySetInnerHTML={{ __html: logicDescription(feature) }} />
+            {feature.excerpt && <p className="excerpt" dangerouslySetInnerHTML={{ __html: logicDescription(feature) }} />}
         </header>
     </article>
 );
