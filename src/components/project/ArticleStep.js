2dad923f3c939edb01cee71495ab89f6d8003ae0
jokokoloko <jhowland82@gmail.com>
2020-01-24 15:29:36 -0800

    Updated ArticleStep.js

--- src/components/project/ArticleStep.js
+++ src/components/project/ArticleStep.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { logicDescription } from '../../logic';
 import Image from '../unit/Image';
 
 const ArticleStep = ({ step }) => (
-    <article id={`step-${step.slug}`} className={`step step-${step.order} col-lg-3`}>
-        <figure className="node-xs-50 d-flex justify-content-center">
-            <Image className="image" source={step.image.fixed} alternate={step.title} fixed />
+    <article id={`step-${step.slug}`} className={`step step-${step.order} col-lg-4`}>
+        <figure className="node-xs-50">
+            <Image className="image" source={step.image.fluid} alternate={step.title} />
         </figure>
-        <header className="node-xs-50 text-center">
-            <p className="excerpt" dangerouslySetInnerHTML={{ __html: logicDescription(step) }} />
+        <header className="node-xs-50">
+            <p className="excerpt" dangerouslySetInnerHTML={{ __html: step.body.childMarkdownRemark.html }} />
         </header>
     </article>
 );
