efec515fb04aa92d2d6beef27cb98fa4c010468c
jokokoloko <jhowland82@gmail.com>
2020-01-26 20:32:17 -0800

    Updated Footer.js

--- src/components/region/Footer.js
+++ src/components/region/Footer.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import useFooter from '../../queries/useFooter';
 import useSite from '../../queries/useSite';
 import Basic from '../section/Basic';
 import Link from '../unit/Link';
+import Logo from '../unit/Logo';
 
 const Footer = ({ offcanvasPush }) => {
     const { name: title } = useSite();
+    const { edges } = useFooter();
+    const loopFooter = edges.map(({ node: collection }) => (
+        <article
+            key={collection.id}
+            id={`collection-${collection.slug}`}
+            className={`collection collection-${collection.slug} col-lg-${collection.column}`}
+        >
+            <section dangerouslySetInnerHTML={{ __html: collection.body.childMarkdownRemark.html }} />
+        </article>
+    ));
     return (
         <footer id="footer" className={offcanvasPush} role="contentinfo">
-            <div className="container-fluid">
-                <Basic id="footer-default" space="space-none" color={6}>
-                    <div className="case">
-                        <p className="copyright">
-                            <Link className="navbar-link" title={title} rel="home">
-                                {title}
-                            </Link>{' '}
-                            &copy; {new Date().getFullYear()}
-                        </p>
-                    </div>
-                </Basic>
-            </div>
+            <Basic space="space-xs-50 space-lg-80" color={6}>
+                <div className="row gutter-50 gutter-lg-80">{loopFooter}</div>
+            </Basic>
+            <section className="navbar navbar-expand-lg color-6">
+                <div className="container justify-content-center">
+                    <Link className="navbar-brand" title={title} rel="home">
+                        <Logo alternate={title} />
+                    </Link>
+                </div>
+            </section>
         </footer>
     );
 };
