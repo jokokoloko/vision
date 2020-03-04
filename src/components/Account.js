e4ccbef85f8be955b298cd60c34020aa6e6251aa
jokokoloko <jhowland82@gmail.com>
2020-03-03 21:49:48 -0800

    Updated Account.js for offcanvas

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Account = () => (
-    <ul className="navbar-action ml-auto account account-guest">
+const Account = ({ offcanvas }) => (
+    <ul className={offcanvas ? 'offcanvas-nav nav flex-column' : 'navbar-action ml-auto account account-guest'}>
         <li className="nav-item">
-            <a className="btn btn-hollow btn-md btn-pill no-class" href="/">
+            <a className={offcanvas ? 'nav-link' : 'btn btn-hollow btn-md btn-pill no-class'} href="/">
                 Register
             </a>
         </li>
         <li className="nav-item">
-            <a className="btn btn-hollow btn-md btn-pill no-class" href="/">
+            <a className={offcanvas ? 'nav-link' : 'btn btn-hollow btn-md btn-pill no-class'} href="/">
                 Log In
             </a>
         </li>
     </ul>
 );
+
+Account.propTypes = {
+    offcanvas: PropTypes.bool,
+};
+
+Account.defaultProps = {
+    offcanvas: false,
+};
 
 export default Account;
